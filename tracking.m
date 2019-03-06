f6b730ffe707e730ca8962af1d78ab1f67fd8d76
Ruben <rubenvanerkelens@hotmail.com>
2019-03-06 17:13:02 +0100

    tracking error

--- tracking.m
+++ tracking.m
@@ -1,20 +1,56 @@
-folder = 'pingpong';
+function [] = tracking(folder)
+
 Files = [dir(fullfile(folder,'*.jpeg')); dir(fullfile(folder,'*.jpg'))];
 
 images = cell(length(Files),1);
+FileNames = cell(length(Files),1);
 for k=1:length(Files)
-   FileNames=Files(k).name;
-   images{k} = imread(FileNames);
-   
-   [H, r, c] = harris_corner_detector(rgb2gray(images{k}), 99, 0.1, 10, 0);
+   FileNames{k} = strcat(folder, '/', Files(k).name);
+   images{k} = imread(FileNames{k});
+end
 
+[~, r, c] = harris_corner_detector(rgb2gray(images{1}), 99, 0.1, 10, 0);
+
+for k=1:length(Files)
+    if k > 1
+        [vx,vy,~,~,~,~] = lucas_kanade(FileNames{k-1}, FileNames{k});
+        size(vy)
+        for i = 1:size(r(:,1))
+            index_r = ceil(r(i,1)/15);
+            index_c = ceil(c(i,1)/15);
+            if index_r > size(vx(1,:))
+                index_r = size(vx(1,:));
+            end
+            if index_c > size(vx(:,1))
+                index_c = size(vx(:,1));
+            end
+            vy(index_c,index_r)
+            r(i,1) = r(i,1) + vy(index_c,index_r);
+            c(i,1) = c(i,1) + vx(index_c,index_r);
+            if r(i,1) < 1
+                r(i,1) = 1;
+            end
+            if r(i,1) > size(images{k}(1,:,1))
+                r(i,1) = size(images{k}(1,:,1));
+            end
+            if c(i,1) < 1
+                c(i,1) = 1;
+            end
+            if c(i,1) > size(images{k}(:,1,1))
+                c(i,1) = size(images{k}(:,1,1));
+            end
+        end
+    end
+    
     cross_size = 10;
-    [x,y,z] = size(images{k});
+    [x,y,~] = size(images{k});
     for i = 1:size(r(:,1))
         left_bound = c(i,1) - cross_size;
         right_bound = c(i,1) + cross_size;
         bottom_bound = r(i,1) - cross_size;
         up_bound = r(i,1) + cross_size;
+        %r(i,1)
+        %c(i,1)
         if left_bound > 0 && right_bound <= x && bottom_bound > 0 && up_bound <= y
             images{k}(left_bound:right_bound,r(i,1),1) = 255;
             images{k}(c(i,1),bottom_bound:up_bound,1) = 255;
@@ -26,36 +62,22 @@ for k=1:length(Files)
     end
 end
 
-%{
-frame0 = imread('person_toy/00000001.jpg');
-
-[H, r, c] = harris_corner_detector(rgb2gray(frame0), 99, 0.1, 10, 0);
-
-cross_size = 10;
-[x,y,z] = size(frame0);
-for i = 1:size(r(:,1))
-    left_bound = c(i,1) - cross_size;
-    right_bound = c(i,1) + cross_size;
-    bottom_bound = r(i,1) - cross_size;
-    up_bound = r(i,1) + cross_size;
-    if left_bound > 0 && right_bound <= x && bottom_bound > 0 && up_bound <= y
-        frame0(left_bound:right_bound,r(i,1),1) = 255;
-        frame0(c(i,1),bottom_bound:up_bound,1) = 255;
-        frame0(left_bound:right_bound,r(i,1),2) = 0;
-        frame0(c(i,1),bottom_bound:up_bound,2) = 0;
-        frame0(left_bound:right_bound,r(i,1),3) = 0;
-        frame0(c(i,1),bottom_bound:up_bound,3) = 0;
-    end
+ % create the video writer with 1 fps
+ writerObj = VideoWriter(strcat(folder, '.avi'));
+ writerObj.FrameRate = 5;
+ % set the seconds per image
+ secsPerImage = 1;
+ % open the video writer
+ open(writerObj);
+ % write the frames to the video
+ for u=1:length(images)
+     % convert the image to a frame
+     frame = im2frame(images{u});
+     for v=1:secsPerImage 
+         writeVideo(writerObj, frame);
+     end
+ end
+ % close the writer object
+ close(writerObj);
+ 
 end
-
-%frame0(r(:,1),c(:,1),:) = 255;
-
-imshow(frame0);
-
-%hold on;
-%plot(r(:,1),c(:,1), 'r+', 'MarkerSize', 20, 'LineWidth', .5);
-%title('Image with cornerpoints');
-
-%}
-
- %writerObj = VideoWriter('myVideo.avi');
