6186710dfc1d5013a6485f8318c59e8941d802f8
AxelBremer <axelbremer94@gmail.com>
2019-03-06 14:11:04 +0100

    Changed color

--- lucas_kanade_demo.m
+++ lucas_kanade_demo.m
@@ -5,12 +5,12 @@ figure
 subplot(1,2,1);
 imshow(image1_synth);
 hold on;
-quiver(axes_synth, axes_synth, vx_synth, vy_synth);
+quiver(axes_synth, axes_synth, vx_synth, vy_synth, 'color',[1 0 0]);
 title('Synth.pgm with flow vector arrows');
 subplot(1,2,2);
 imshow(image1_sphere);
 hold on;
-quiver(axes_sphere, axes_sphere, vx_sphere, vy_sphere);
+quiver(axes_sphere, axes_sphere, vx_sphere, vy_sphere, 'color',[1 0 0]);
 title('Sphere.ppm with flow vector arrows');
 
 figure
@@ -18,7 +18,7 @@ subplot(2,3,1);
 imshow(image1_synth);
 title('Original Synth1.pgm');
 subplot(2,3,2);
-quiver(axes_synth, axes_synth, vx_synth, vy_synth);
+quiver(axes_synth, -axes_synth, vx_synth, -vy_synth, 'color',[1 0 0]);
 title('Flow vectors of difference Synth1.pgm and Synth1.pgm');
 subplot(2,3,3);
 imshow(image2_synth);
@@ -27,7 +27,7 @@ subplot(2,3,4);
 imshow(image1_sphere);
 title('Original Sphere1.ppm');
 subplot(2,3,5);
-quiver(axes_sphere, axes_sphere, vx_sphere, vy_sphere);
+quiver(axes_sphere, -axes_sphere, vx_sphere, -vy_sphere, 'color',[1 0 0]);
 title('Flow vectors of difference Sphere1.ppm and Sphere1.ppm');
 subplot(2,3,6);
 imshow(image1_sphere);
